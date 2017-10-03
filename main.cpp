6e6f183c3ba3e7b39b5525f8b169e244fe596b1e
chiri1125 <SEMBLANTESG@cslab-5.acldomain.local>
2017-10-03 16:45:08 -0400

    second program

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include "sample.h"
+using namespace exercises;
+
+int main()
+{
+  Exercise ex; // exercise object
+  ex.x = 3; //
+  ex.y =" Hello";
+  ex.print();
+  std::cout << " \n";
+
+  return 0;
+
+}
