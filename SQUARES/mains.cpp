247a1d5230c4e796aeea3c530405ff112ec72f12
chiri1125 <SEMBLANTESG@cslab-5.acldomain.local>
2017-10-10 17:50:26 -0400

    Class

---
+++ SQUARES/mains.cpp
@@ -0,0 +1,13 @@
+#include "squares.h"
+using namespace shapes;
+
+int main()
+{
+  Square1 sq1;// invoking the default constructor of Square1
+  Square2 sq2;// invoking the default constructor of square2
+
+  Square1 sq3(5);
+  Square2 sq4(4);
+
+  return 0;
+}
